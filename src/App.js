a65211abf5b848ff3dbaf6a0bb673f439576464c
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2021-05-20 12:19:31 +0200

    Added a toggle button to enable/disable the webcam

--- src/App.js
+++ src/App.js
@@ -1,12 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import Webcam from "react-webcam";
 
+const webcamDimensions = {
+  width: 300,
+  height: 300,
+};
+
+const videoConstraints = {
+  ...webcamDimensions,
+};
+
 function App() {
+  const [webcamDisabled, setWebcamDisabled] = useState(true);
+
+  function toggleDisableWebcam() {
+    setWebcamDisabled(!webcamDisabled);
+  }
+
   return (
     <>
       <h1>My Webcam :)</h1>
 
-      <Webcam />
+      <div>
+        <button onClick={toggleDisableWebcam}>ENABLE/DISABLE</button>
+      </div>
+
+      {webcamDisabled ? (
+        <div
+          style={{
+            width: `${webcamDimensions.width}px`,
+            height: `${webcamDimensions.width}px`,
+            backgroundColor: "black",
+          }}
+        />
+      ) : (
+        <Webcam videoConstraints={videoConstraints} />
+      )}
     </>
   );
 }
